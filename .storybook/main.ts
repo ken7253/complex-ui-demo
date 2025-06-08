754c66d86fc8903f8daffcd01ebf8f52732f8ea1
ken7253 <ken7253@gmail.com>
2025-06-08 19:31:29 +0900

    restart

--- .storybook/main.ts
+++
@@ -1,8 +0,0 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-
-const config: StorybookConfig = {
-  stories: ['../demo/**/*.stories.tsx'],
-  framework: '@storybook/react-vite',
-};
-
-export default config;
--- demo/InteractiveFlow/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const InteractiveFlow = () => {
-  return <div>test</div>;
-};
